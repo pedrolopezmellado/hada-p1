929e319f600d05723c674862be85c35a61ac9340
Pedro López <pedrolome@outlook.es>
2020-02-12 14:29:25 +0100

    Cambio en Program.cs

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,6 +10,27 @@ namespace hada_p1
 	{
 		static void Main(string[] args)
 		{
+			do
+			{
+				string men;
+				System.Console.WriteLine("De que unidad partes? (s/m)");
+				men = System.Console.ReadLine();
+				System.Console.WriteLine("Cantidad: ");
+				string cant = System.Console.ReadLine();
+				double tiempo = System.Double.Parse(cant);
+				double sol = 0.0;
+				if (men == "s")
+				{
+					sol = HadaP1.Seconds2Minutes(tiempo);
+					System.Console.WriteLine("Tiempo de segundos a minutos: " + sol + "\n");
+				}
+				else if (men == "m")
+				{
+					sol = HadaP1.Minutes2Seconds(tiempo);
+					System.Console.WriteLine("Tiempo de minutos a segundos: " + sol + "\n");
+				}
+				System.Console.WriteLine("Quiere realizar más conversiones? (s/n)");
+			} while (System.Console.ReadLine() == "s");
 		}
 	}
 }

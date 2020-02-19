66cf11922ec13a6f6b7546382da560e45fe7f728
Pedro López <pedrolome@outlook.es>
2020-02-19 16:33:47 +0100

    Modificado HadaP1.cs

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -8,21 +8,21 @@ namespace hada_p1
 {
 	class HadaP1
 	{
-		public static double Seconds2Minutes(double s)
+		public static double Seconds2Minutes(double s) // este método pasa de segundos a minutos
 		{
-			if (s == 0)
+			if (s == 0) // si es 0 devuelve directamente 0
 			{
 				return 0;
 			}
 			else
 			{
-				return s / 60.0;
+				return s / 60.0; // divide entre 60
 			}
-		}
+		} // 
 
-		public static double Minutes2Seconds(double m)
+		public static double Minutes2Seconds(double m) // este método pasa de minutos a segundos
 		{
-			return m * 60;
+			return m * 60; // multiplica por 60
 		}
 	}
 }
